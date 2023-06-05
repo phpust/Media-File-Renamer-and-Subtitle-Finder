c35284fd160529d5d17b4e14264ecf2de995c698
sajad kashizadeh <s.kashizadeh@gmail.com>
2023-06-05 17:21:10 +0330

    minor bug fix

--- Batches/rename_movie_batch.py
+++ Batches/rename_movie_batch.py
@@ -1,5 +1,19 @@
-import rename_movie
+import os
 import sys
+ 
+# getting the name of the directory
+# where the this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+ 
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+ 
+# adding the parent directory to
+# the sys.path.
+sys.path.append(parent)
+
+import rename_movie
 
 file_path = sys.argv[1]
 rename_movie.main(file_path)
